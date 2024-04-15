b9a9dea332af36d1195ca3d35231a5399dd96490
Vidh3566 <Vidh3566@CSL0710.global.chainsys.com>
2024-04-15 21:58:22 +0530

    4th challenge

---
+++ Coding/src/com/chainsys/codingchallenge/Challenge4.java
@@ -0,0 +1,29 @@
+package com.chainsys.codingchallenge;
+
+import java.util.Scanner;
+
+/*Input
+ 42
+3.1415
+Welcome to HackerRank's Java tutorials!
+Output
+String: Welcome to HackerRank's Java tutorials!
+Double: 3.1415
+Int: 42
+*/
+public class Challenge4 {
+	public static void main(String[] args) {
+		 Scanner scan = new Scanner(System.in);
+	        int i = scan.nextInt();
+	        double d = scan.nextDouble();
+	        scan.nextLine();
+	        String s = scan.nextLine();
+	       
+
+	        // Write your code here.
+
+	        System.out.println("String: " + s);
+	        System.out.println("Double: " + d);
+	        System.out.println("Int: " + i);
+	}
+}
